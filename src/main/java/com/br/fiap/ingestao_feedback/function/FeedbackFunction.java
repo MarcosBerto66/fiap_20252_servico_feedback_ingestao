a6b71e09908acbeac7b97bdeffb0b5141783a39c
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2026-01-03 18:41:23 -0300

    fix: ajusta a checagem da validação do dto

--- src/main/java/com/br/fiap/ingestao_feedback/function/FeedbackFunction.java
+++ src/main/java/com/br/fiap/ingestao_feedback/function/FeedbackFunction.java
@@ -3,9 +3,15 @@ package com.br.fiap.ingestao_feedback.function;
 import com.br.fiap.ingestao_feedback.dto.FeedbackDTO;
 import com.br.fiap.ingestao_feedback.model.Feedback;
 import com.br.fiap.ingestao_feedback.service.IngestionService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
+import java.util.List;
 import java.util.function.Function;
 
 @Configuration
@@ -16,10 +22,27 @@ public class FeedbackFunction {
     public FeedbackFunction(IngestionService ingestionService) {
         this.ingestionService = ingestionService;
     }
+
     // MS 1: Ingestão (Recebe FeedbackDTO -> Retorna Feedback salvo)
     @Bean("processarFeedback")
-    public Function<FeedbackDTO, Feedback> processarFeedback() {
+    public Function<FeedbackDTO, Message<Object>> processarFeedback() {
         System.out.println(">>> [DEBUG] O BEAN 'processarFeedback' FOI INICIALIZADO! <<<");
-        return ingestionService::processar;
+
+        return dto -> {
+            try {
+                Feedback feedback = ingestionService.processar(dto);
+                return MessageBuilder.withPayload((Object) feedback)
+                        .setHeader("statusCode", HttpStatus.CREATED.value())
+                        .build();
+
+            } catch (ConstraintViolationException ex) {
+                List<String> errors = ex.getConstraintViolations().stream()
+                        .map(ConstraintViolation::getMessage)
+                        .toList();
+                return MessageBuilder.withPayload((Object) errors)
+                        .setHeader("statusCode", HttpStatus.BAD_REQUEST.value())
+                        .build();
+            }
+        };
     }
 }
--- src/test/java/com/br/fiap/ingestao_feedback/function/FeedbackFunctionTest.java
+++ src/test/java/com/br/fiap/ingestao_feedback/function/FeedbackFunctionTest.java
@@ -3,8 +3,14 @@ package com.br.fiap.ingestao_feedback.function;
 import com.br.fiap.ingestao_feedback.dto.FeedbackDTO;
 import com.br.fiap.ingestao_feedback.model.Feedback;
 import com.br.fiap.ingestao_feedback.service.IngestionService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.messaging.Message;
 
+import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,10 +27,36 @@ class FeedbackFunctionTest {
         Feedback expected = new Feedback("ok", 6);
         when(ingestion.processar(dto)).thenReturn(expected);
 
-        Function<FeedbackDTO, Feedback> f = function.processarFeedback();
-        Feedback result = f.apply(dto);
+        Function<FeedbackDTO, Message<Object>> f = function.processarFeedback();
+        Message<Object> result = f.apply(dto);
 
-        assertSame(expected, result);
+        assertSame(expected, result.getPayload());
+        assertEquals(result.getHeaders().get("statusCode"), HttpStatus.CREATED.value());
         verify(ingestion, times(1)).processar(dto);
+    }
+
+    @Test
+    void processarFeedback_handlesConstraintViolationException() {
+        IngestionService ingestion = mock(IngestionService.class);
+        FeedbackFunction function = new FeedbackFunction(ingestion);
+
+        FeedbackDTO dto = new FeedbackDTO("ok", 12);
+
+        ConstraintViolation<?> violation = mock(ConstraintViolation.class);
+        when(violation.getMessage()).thenReturn("A nota máxima é 10");
+
+        ConstraintViolationException exception = new ConstraintViolationException(Set.of(violation));
+
+        when(ingestion.processar(dto)).thenThrow(exception);
+
+        Function<FeedbackDTO, Message<Object>> f = function.processarFeedback();
+        Message<Object> response = f.apply(dto);
+
+        Object body = response.getPayload();
+        assertInstanceOf(List.class, body);
+        List<?> errors = (List<?>) body;
+        assertEquals(1, errors.size());
+        assertEquals("A nota máxima é 10", errors.get(0));
+        assertEquals(HttpStatus.BAD_REQUEST.value(), response.getHeaders().get("statusCode"));
     }
 }
